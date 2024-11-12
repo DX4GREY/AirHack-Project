10db37d320f5f28c35fd18518c83e149233a079e
Dx4Grey <dxablack@gmail.com>
2024-11-12 23:15:34 +0700

    InterfaceManager.java: Fixing invalid channel or frequency.

--- app/src/main/java/com/dxablack/InterfaceManager.java
+++ app/src/main/java/com/dxablack/InterfaceManager.java
@@ -90,7 +90,7 @@ public class InterfaceManager {
             return (freq - 2407) / 5;
         }else if (freq >= 5170 && freq <= 5835){
             return  (freq - 500) / 5;
-        }else if (freq == 0) {
+        }else if (freq <= 0) {
             return 0;
         }else {
             throw new IllegalArgumentException("Frequency not within WiFi bands");
@@ -101,7 +101,7 @@ public class InterfaceManager {
             return 2407 + channel * 5;
         }else if (channel >= 32 && channel <= 177){
             return 5000 + channel * 5;
-        }else if (channel == 0) {
+        }else if (channel <= 0) {
             return 0;
         }else {
             throw new IllegalArgumentException("Channel not within WiFi bands");
